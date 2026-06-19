19c89643666c33dcb8f7d10ff196dc8dcd548ed4
bryanfryan <bryan.wu0817@gmail.com>
2026-06-19 15:37:50 -0400

    added an introduction
    intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Bryan Wu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+print("Hi everyone! My name is Bryan, and I am currently a junior in a High School in New Jersey. A fun fact about myself is that I love to play volleyball!")
 if __name__ == "__main__":
     main()
